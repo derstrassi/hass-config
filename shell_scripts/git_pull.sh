6ea683c8b659d9e4474db2c98b3d76720468f836
Dennis Straßer <dennis.strasser@gmail.com>
2018-12-26 20:04:28 +0100

    fix paths

--- shell_scripts/git_pull.sh
+++ shell_scripts/git_pull.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd ~/.homeassistant
 git fetch && git reset --hard origin/master
--- shell_scripts/git_update_new_commits_sensor.sh
+++ shell_scripts/git_update_new_commits_sensor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd ~/.homeassistant
 git fetch origin master
