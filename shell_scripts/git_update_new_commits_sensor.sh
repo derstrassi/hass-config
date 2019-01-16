1ae6b62a6c7fddc435e2bfd26dc591a3e5cef478
Dennis Straßer <dennis.strasser@gmail.com>
2019-01-16 15:34:59 +0100

    update

--- shell_scripts/git_update_new_commits_sensor.sh
+++ shell_scripts/git_update_new_commits_sensor.sh
@@ -1,7 +1,5 @@
 cd /root/.homeassistant
 git fetch origin master
 commits="$(git rev-list --count master..origin/master)"
-#pw="$(grep HTTP_API_PASSWORD secrets.yaml | sed 's/HTTP_API_PASSWORD://')"
-
-#curl -X POST -H "x-ha-access: $pw" -H "Content-Type: application/json" -d "{\"state\": $commits}" "http://127.0.0.1:8123/api/states/sensor.new_commits"
 curl -X POST -H "Content-Type: application/json" -d "{\"state\": $commits}" "http://127.0.0.1:8123/api/states/sensor.new_commits"
+exit 0
