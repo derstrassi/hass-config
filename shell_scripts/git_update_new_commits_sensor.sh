cdb1681c97ec1933a76bde717e2f8893a370c1ca
Dennis Straßer <dennis.strasser@gmail.com>
2019-01-17 14:28:55 +0100

    rework git

--- shell_scripts/git_update_new_commits_sensor.sh
+++
@@ -1,4 +0,0 @@
-cd /root/.homeassistant
-/usr/local/bin/git fetch origin master
-commits=`/usr/local/bin/git rev-list --count master..origin/master`
-/usr/local/bin/curl -X POST -H "Content-Type: application/json" -d "{\"state\": $commits}" "http://127.0.0.1:8123/api/states/sensor.new_commits"
